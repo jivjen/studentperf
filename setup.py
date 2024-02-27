f8333a95d1e05e4f9fb8e9115b79d27e36c4c62c
jivjen <jival.jisil@yahoo.co.in>
2024-02-27 22:45:22 +0530

    Setting up setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import find_packages, setup
+from typing import List
+
+
+HYPHEN = '-e .'
+def get_requirements(file_path: str) -> List[str]:
+
+    requirements=[]
+    with open('requirements.txt') as file_obj:
+        requirements = file_obj.readlines()
+        [req.replace("\n","") for req in requirements]
+        if HYPHEN in requirements:
+            requirements.remove(HYPHEN)
+        return requirements
+
+setup(
+    name="mlproject",
+    version='0.0.1',
+    author='Jival',
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
+)
